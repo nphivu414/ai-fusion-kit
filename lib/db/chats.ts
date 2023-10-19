6c3f9fdf3bbafb2d30545e2c8dc8c2526e65ce4f
Vu Nguyen <nphivu414@gmail.com>
2023-10-19 11:01:54 +0700

    - Test react cache

--- lib/db/chats.ts
+++ lib/db/chats.ts
@@ -29,6 +29,8 @@ export const getChats = cache(async (supabase: SupabaseClient<Database>, params:
     return null
   }
 
+  await new Promise(r => setTimeout(r, 10000));
+
   log.info(`${getChats.name} fetched successfully`, { data });
   return data
 })
