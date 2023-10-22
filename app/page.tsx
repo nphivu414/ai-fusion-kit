05df6f509c5d78a90c71e6ade9469688ac0f4b2e
Vu Nguyen <nphivu414@gmail.com>
2023-10-22 18:15:40 +0700

    - Update homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import { ChatAppFeatures } from "@/components/modules/home/ChatAppFeatures";
 import { DescriptionHeadingText } from "@/components/modules/home/DescriptionHeadingText";
 import { FeatureItems } from "@/components/modules/home/FeatureItems";
 import { HeroBannerImage } from "@/components/modules/home/HeroBannerImage";
@@ -14,14 +13,14 @@ export default async function Home() {
       <NavigationBar/>
       <section className="space-y-6 pb-8 pt-6 md:pb-12 md:pt-10 lg:py-24">
         <div className="container flex max-w-[64rem] flex-col items-center gap-6 text-center">
-          <h1 className="font-heading text-3xl sm:text-5xl md:text-6xl">
+          <h1 className="font-heading bg-gradient-to-r from-gray-400 to-gray-700 bg-clip-text text-3xl font-bold leading-[1.1] tracking-tighter text-transparent dark:from-white dark:to-gray-500 sm:text-5xl md:text-6xl">
            AI Fusion Kit
           </h1>
           <DescriptionHeadingText/>
           <div className="flex items-center space-x-4">
             <Button variant="outline"><Play className="mr-2" size={16}/> Demo</Button>
-            <Link href="/apps/chat" className={cn(buttonVariants())}>
-              Signup
+            <Link href="/apps/chat/new" className={cn(buttonVariants())}>
+              Get Started
             </Link>
           </div>
         </div>
@@ -32,8 +31,8 @@ export default async function Home() {
         className="container space-y-6 bg-slate-50 py-8 dark:bg-transparent md:py-12 lg:py-24"
       >
         <div className="mx-auto flex max-w-[58rem] flex-col items-center space-y-4 text-center">
-          <h2 className="font-heading text-3xl leading-[1.1] sm:text-4xl">
-            Next-Gen Web Tools for Chatbot App
+          <h2 className="font-heading bg-gradient-to-r from-gray-400 to-gray-700 bg-clip-text text-3xl font-bold leading-[1.1] tracking-tighter text-transparent dark:from-white dark:to-gray-500 sm:text-4xl">
+            Modern Web Tools for Chatbot App
           </h2>
           <p className="leading-normal text-muted-foreground sm:text-lg sm:leading-7">
             Revolutionize the way you build chatbot applications with the power of Next.js, Server Components, and Supabase. This template provides you with a solid foundation to create cutting-edge chatbot apps that are feature-rich and responsive, all while taking advantage of the latest Next.js technology.
@@ -41,7 +40,6 @@ export default async function Home() {
         </div>
         <FeatureItems/>
       </section>
-      <ChatAppFeatures/>
     </>
   )
 }
