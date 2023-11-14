9b4ba6344b0e93b80951ef50e3f96dca9759a393
Vu Nguyen <nphivu414@gmail.com>
2023-11-14 11:44:05 +0700

    - Fix type errors - Update lint fixes

--- components/theme/ThemeProvider.tsx
+++ components/theme/ThemeProvider.tsx
@@ -1,9 +1,9 @@
-"use client"
+"use client";
 
-import React from "react"
-import { ThemeProvider as NextThemesProvider } from "next-themes"
-import { ThemeProviderProps } from "next-themes/dist/types"
+import React from "react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { ThemeProviderProps } from "next-themes/dist/types";
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
