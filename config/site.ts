99703a1ea96bc3efa2ff8a861ac0f7ba239479a8
Vu Nguyen <nphivu414@gmail.com>
2024-02-25 10:42:42 +0700

    - Upgrade dependecies

--- config/site.ts
+++ config/site.ts
@@ -1,22 +1,23 @@
-import { env } from "@/env.mjs"
+import { env } from "@/env.mjs";
 
-export type SiteConfig = typeof siteConfig
+export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
   name: "AI Fusion Kit",
-  description: "A feature-rich, highly customizable AI Chatbot Template, empowered by Next.js.",
+  description:
+    "A feature-rich, highly customizable AI Chatbot Template, empowered by Next.js.",
   links: {
     x: "https://twitter.com/nphivu414",
     github: "https://github.com/nphivu414/ai-fusion-kit",
     docs: "",
   },
-}
+};
 
 export const getURL = () => {
-  let url = env.NEXT_PUBLIC_APP_URL || 'http://localhost:3000/'
+  let url = env.NEXT_PUBLIC_APP_URL || "http://localhost:3000/";
   // Make sure to include `https://` when not localhost.
-  url = url.includes('http') ? url : `https://${url}`
+  url = url.includes("http") ? url : `https://${url}`;
   // Make sure to include a trailing `/`.
-  url = url.charAt(url.length - 1) === '/' ? url : `${url}/`
-  return url
-}
+  url = url.charAt(url.length - 1) === "/" ? url : `${url}/`;
+  return url;
+};
