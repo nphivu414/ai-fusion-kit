1d250ee29b7291f9b69b09c3a427957bc0d0fbb0
Vu Nguyen <nphivu414@gmail.com>
2024-06-26 18:34:50 +0700

    chore: Add database migration scripts for username migration and handling new users (#20)

---
+++ supabase/migrations/20240626065226_update_handle_new_user.sql
@@ -0,0 +1,17 @@
+CREATE OR REPLACE FUNCTION public.handle_new_user()
+ RETURNS trigger
+ LANGUAGE plpgsql
+ SECURITY DEFINER
+AS $function$
+begin
+  insert into public.profiles (id, full_name, avatar_url, username)
+  values (
+    new.id, 
+    new.raw_user_meta_data->>'full_name', 
+    new.raw_user_meta_data->>'avatar_url',
+    new.email
+  );
+  return new;
+end;
+$function$
+;
